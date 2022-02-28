790e27563993ae9f21dd64a55e465373677cc6e5
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-02-28 21:26:17 +0100

    [AFT-5] feat(Book): Added book model

---
+++ src/models/Book.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+require("mongoose-schema-jsonschema")(mongoose);
+const Metadata = require("./Metadata");
+
+const { Schema, model } = mongoose;
+
+const bookSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    metadata: {
+      type: Metadata,
+      required: true
+    }
+  },
+  { versionKey: false }
+);
+
+module.exports = model("Book", bookSchema);
