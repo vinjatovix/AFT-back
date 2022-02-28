9e6df864cbde9895960a451dddf83282ef2c5b35
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-03-01 00:15:02 +0100

    [AFT-10] feat(db): added basic bd error management system

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { DB } = require("../messages");
+const { throwError } = require("./throwError");
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_REPLICA_SET } = process.env;
 const password = encodeURIComponent(DB_PASSWORD);
@@ -14,4 +15,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+module.exports = { connectDB, throwError };
