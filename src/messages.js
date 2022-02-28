0f3e8b9c36e5aee1c158ce6d916a9199c422125d
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-02-28 20:26:53 +0100

    [AFT-3] feat(chore): add basic koa server. Closed #3

---
+++ src/messages.js
@@ -0,0 +1,21 @@
+module.exports = {
+  DB: {
+    CONNECTION: {
+      OK: "Connection to database successfully"
+    }
+  },
+  HTTP: {
+    RESPONSES: {
+      200: "OK",
+      201: "Created",
+      204: "No content",
+      400: "Bad Request",
+      401: "Unauthorized",
+      403: "Forbidden",
+      404: "Not Found",
+      409: "Conflict",
+      422: "Unprocessable Entity",
+      500: "Internal Server Error"
+    }
+  }
+};
