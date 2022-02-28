3323f93a67ee759d4959127c2172d95920c8141e
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-02-28 21:26:17 +0100

    [AFT-5] feat(Book): Added book model

---
+++ src/api/common/shared/index.js
@@ -0,0 +1,10 @@
+const createMetadata = (createdBy, createdAt = new Date(), updatedAt = null, updatedBy = null) => ({
+  createdAt,
+  createdBy,
+  updatedAt: updatedAt || createdAt,
+  updatedBy: updatedBy || createdBy
+});
+
+module.exports = {
+  createMetadata
+};
