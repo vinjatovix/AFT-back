6a759169438045683752dc720ab6dde7b0d959d9
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-03-01 20:07:58 +0100

    [AFT-11] feat(Service): added winston logger. Closed #11

---
+++ src/service/logger.js
@@ -0,0 +1,21 @@
+const winston = require("winston");
+
+// { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" })
+  ]
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  );
+}
+
+module.exports = logger;
--- src/service/service.js
+++ src/service/service.js
@@ -1,12 +1,13 @@
 const Koa = require("koa");
+const koaLogger = require("koa-logger-winston");
 const json = require("koa-json");
 const bodyParser = require("koa-bodyparser");
+const logger = require("./logger");
 const { errorHandler } = require("../middlewares");
-
 const bookRouter = require("../api/book/router").routes();
 
 const service = new Koa();
 
-service.use(json()).use(bodyParser()).use(errorHandler).use(bookRouter);
+service.use(koaLogger(logger)).use(json()).use(bodyParser()).use(errorHandler).use(bookRouter);
 
 module.exports = service;
