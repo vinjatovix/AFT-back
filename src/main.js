29bdfae6cf20b0a5fb7033a2c6d2796d26c87b80
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-03-01 21:44:58 +0100

    [AFT-13] chore: Added migrations by version system. Closed #13

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+const migrations = require("../migrations");
 const service = require("./service/service");
 const { connectDB } = require("./db");
 
 const { PORT } = process.env;
 
+migrations.up();
 connectDB();
 
 service.listen(PORT);
