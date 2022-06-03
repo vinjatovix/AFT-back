e76aeeb72b82b784d7f86562cd87271e420236d5
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-06-03 17:08:58 +0200

    [AFT-34] feat(Book) initial CRUD

--- src/api/book/controller.js
+++ src/api/book/controller.js
@@ -11,7 +11,27 @@ const findAll = async (ctx, next) => {
   ctx.body = await Service.findAll();
   await next();
 };
+
+const findBySlug = async (ctx, next) => {
+  ctx.body = await Service.findBySlug(ctx.params.slug);
+  await next();
+};
+
+const findOneAndUpdate = async (ctx, next) => {
+  ctx.body = await Service.findOneAndUpdate(ctx.params.slug, ctx.request.body, ctx.state.userData.username);
+  await next();
+};
+
+const findOneAndDelete = async (ctx, next) => {
+  ctx.body = await Service.findOneAndDelete(ctx.params.slug, ctx.state.userData.username);
+  ctx.status = httpStatusCodes.HTTP_NO_CONTENT;
+  await next();
+};
+
 module.exports = {
   create,
-  findAll
+  findAll,
+  findBySlug,
+  findOneAndUpdate,
+  findOneAndDelete
 };
---
+++ test/unit/api/book/update.test.js
@@ -0,0 +1,36 @@
+const httpRequest = require("../../../fixtures/httpRequest")();
+const Repository = require("../../../../src/api/book/repository");
+const getUrl = slug => `/api/v1/book/${slug}`;
+
+describe("Book module - update", () => {
+  beforeEach(() => {
+    Repository.findOneAndUpdate = jest.fn(async payload => {
+      return payload;
+    });
+  });
+  it("should update a book", async () => {
+    const slug = "uno";
+
+    const { status } = await httpRequest("PATCH", getUrl(slug), {
+      name: "Uno"
+    });
+
+    expect(status).toBe(200);
+  });
+
+  it("should fail because aft.user is not valid role", async () => {
+    const slug = "uno";
+
+    const { status, body } = await httpRequest("PATCH", getUrl(slug), { name: "Uno" }, "user");
+
+    expect(status).toBe(403);
+    expect(body).toMatchObject({
+      module: "authorization",
+      code: "E4",
+      id: "INVALID_ROLE",
+      message: "Role not allowed",
+      status: 403,
+      level: "error"
+    });
+  });
+});
