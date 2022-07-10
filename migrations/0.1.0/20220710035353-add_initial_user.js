52571c9d400fecce5551a84185da6aacbc8a5f37
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-07-10 06:24:20 +0200

    [AFT-59] feat(User): migration add admin user

---
+++ migrations/0.1.0/20220710035353-add_initial_user.js
@@ -0,0 +1,17 @@
+const { createMetadata } = require("../../src/api/common/shared");
+const logger = require("../../src/service/logger");
+const { encryptPassword } = require("../../src/services/passwordEncryption");
+
+module.exports = {
+  async up(db) {
+    const time = +new Date();
+    await db.collection("users").insertOne({
+      username: process.env.ADMIN_USERNAME,
+      password: encryptPassword(process.env.ADMIN_PASSWORD),
+      roles: ["aft-admin", "aft-user", "aft-editor"],
+      metadata: createMetadata("GOD")
+    });
+
+    logger.info(`Migration "add_initial_user" finished in ${+new Date() - time}ms`);
+  }
+};
