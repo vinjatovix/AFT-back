fd83d917bcee7af0124f6cb9715453a0b26a4584
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-07-18 22:33:58 +0200

    [AFT-77] doc(Scene): update swagger

--- src/api/scene/router.js
+++ src/api/scene/router.js
@@ -1,5 +1,8 @@
 const KoaRouter = require("koa-router");
 const { authAnyRole, authEditor, getCredentials, tokenAuth } = require("../../middlewares/authorization");
+const { filter } = require("../../middlewares/filter");
+const { include } = require("../../middlewares/include");
+const { validateBody } = require("../../middlewares/validateBody");
 const Controller = require("./controller");
 
 const sceneRouter = new KoaRouter();
@@ -7,10 +10,10 @@ const sceneRouter = new KoaRouter();
 sceneRouter
   .prefix("/api/v1/scene")
   .use(tokenAuth, getCredentials)
-  .get("/", authAnyRole, Controller.findAll)
-  .get("/:slug", authAnyRole, Controller.findBySlug)
-  .post("/", authEditor, Controller.create)
-  .patch("/:slug", authEditor, Controller.findOneAndUpdate)
+  .get("/", authAnyRole, include, filter, Controller.findAll)
+  .post("/", authEditor, include, validateBody, Controller.create)
+  .get("/:slug", authAnyRole, include, Controller.findBySlug)
+  .patch("/:slug", authEditor, include, validateBody, Controller.findOneAndUpdate)
   .del("/:slug", authEditor, Controller.findOneAndDelete);
 
 module.exports = sceneRouter;
--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose");
 const { DB } = require("../messages");
 const logger = require("../service/logger");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_REPLICA_SET, NODE_ENV } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE } = process.env;
 const password = encodeURIComponent(DB_PASSWORD);
 
 const base = `${DB_USER}:${password}@${DB_HOST}/${DB_DATABASE}`;
 
-const connectionString =
-  NODE_ENV === "development"
-    ? `mongodb://${base}?authSource=${DB_DATABASE}&ssl=false&replicaSet=${DB_REPLICA_SET}`
-    : `mongodb+srv://${base}?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${base}?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
   try {
